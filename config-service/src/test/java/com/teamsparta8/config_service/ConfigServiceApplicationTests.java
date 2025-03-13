766f1faeefcaa181cb6d76bb4519504668bd75f8
chaewKim <anjh7127@gmail.com>
2025-03-13 20:24:29 +0900

    init

---
+++ config-service/src/test/java/com/teamsparta8/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsparta8.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
