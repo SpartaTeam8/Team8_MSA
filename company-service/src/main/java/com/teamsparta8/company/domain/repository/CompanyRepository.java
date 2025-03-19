a6c787969dcf2753d11c5019a27fc57c93b89618
MyYeonbi <yeonbi_kim@icloud.com>
2025-03-19 20:01:07 +0900

    Feat: 검색 API 설계 중 CompanyRepository생성

--- company-service/src/main/java/com/teamsparta8/company/domain/repository/CompanyRepository.java
+++ company-service/src/main/java/com/teamsparta8/company/domain/repository/CompanyRepository.java
@@ -2,10 +2,12 @@ package com.teamsparta8.company.domain.repository;
 
 import com.teamsparta8.company.domain.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, Long>,
+    JpaSpecificationExecutor<Company> {
   boolean existsByName(String name);
   boolean existsByContactEmail(String contactEmail);
 }
