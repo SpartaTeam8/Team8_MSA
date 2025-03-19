d6e7d7bb86048475cfc43148fb82e2b15bf46f57
MyYeonbi <yeonbi_kim@icloud.com>
2025-03-19 18:49:37 +0900

    Feat: 검색 API 설계 중 Controller 생성

--- company-service/src/main/java/com/teamsparta8/company/presentation/controller/CompanyController.java
+++ company-service/src/main/java/com/teamsparta8/company/presentation/controller/CompanyController.java
@@ -3,13 +3,17 @@ package com.teamsparta8.company.presentation.controller;
 import com.teamsparta8.company.application.dto.CompanyRequestDto;
 import com.teamsparta8.company.application.dto.CompanyResponseDto;
 import com.teamsparta8.company.application.service.CompanyService;
+import com.teamsparta8.company.domain.model.CompanyType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +26,17 @@ public class CompanyController {
   public ResponseEntity<CompanyResponseDto> createCompany(@Valid @RequestBody CompanyRequestDto requestDto) {
     CompanyResponseDto responseDto = companyService.createCompany(requestDto);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<CompanyResponseDto>> searchCompanies(
+      @RequestParam(required = false) String name,
+      @RequestParam(required = false) CompanyType type,
+      @RequestParam(required = false) Long hubId,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+
+    Page<CompanyResponseDto> companies = companyService.searchCompanies(name, type, hubId, page, size);
+    return ResponseEntity.ok(companies);
   }
 }
